1b5bb616dfe6c364b7d3a0142985ffd2ea519baf
Fritte91 <fredriklindberg1991@gmail.com>
2025-05-17 09:53:47 +0200

    not safe push

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
     ],
     domains: ['ylvgvgkyawmialfcudex.supabase.co'],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -26,4 +30,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
