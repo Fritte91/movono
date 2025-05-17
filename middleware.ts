1f2aacbeb94860a814f554cef3f1feb5b2e1106e
Fritte91 <fredriklindberg1991@gmail.com>
2025-05-17 11:00:07 +0200

    tried to fix so front page show up

--- middleware.ts
+++ middleware.ts
@@ -36,9 +36,9 @@ export async function middleware(request: NextRequest) {
   try {
     const { data: { session } } = await supabase.auth.getSession()
 
-    // If user is not signed in and the current path is not /login or /signup
+    // If user is not signed in and the current path is not /login, /signup, or /
     // redirect the user to /login
-    if (!session && !['/login', '/signup'].includes(request.nextUrl.pathname)) {
+    if (!session && !['/login', '/signup', '/'].includes(request.nextUrl.pathname)) {
       return NextResponse.redirect(new URL('/login', request.url))
     }
 
